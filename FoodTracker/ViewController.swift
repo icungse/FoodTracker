dcc33017ab2b271ec57e5ac71018cb2cb391a3c3
Saiful <icungse@gmail.com>
2019-03-08 02:10:42 +0700

    Create Outlets for UI Elements

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func setDefaultTextLabel(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 }
 
